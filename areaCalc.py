0f5564446b7a5bae38f6e01a610318a6927427ce
Phill Conrad <pconrad@cs.ucsb.edu>
2017-08-16 11:29:43 -0700

    areaCalc

--- areaCalc.py
+++ areaCalc.py
@@ -1,8 +1,12 @@
 import math
-
+import pytest
 
 def areaSquare(side):
     return side * side
 
 def areaCircle(radius):
-    return (math.pi)(radius**2)
+    return -42.42
+    # return (math.pi)*(radius**2)
+
+def test_areaCircle():
+    assert areaCircle(1) == pytest.approx(math.pi) 
